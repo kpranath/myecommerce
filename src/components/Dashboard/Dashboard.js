ea7265b3e4c4cd64e09f39eecad9edcf0082c2e3
kpranath <kpranath@gmail.com>
2022-08-07 22:26:47 +0530

    nothing much added

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../../App.css'
+
+class Dashboard extends Component {
+  render() {
+    return (
+        <div className='App-header'>
+            <p>This is dashboard</p>
+        </div>
+    )
+  }
+}
+
+export default Dashboard
